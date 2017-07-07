4018183342bf44256a3a562b36570ae781e01bc2
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-07 17:57:52 -0400

    first commit

--- TipCalculator-App/ViewController.m
+++ TipCalculator-App/ViewController.m
@@ -9,20 +9,49 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (assign, nonatomic) CGFloat defaultTipPercentage;
 
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.defaultTipPercentage = 0.15;
+
+
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)calculateTipButton:(UIButton *)sender {
+    
+    float billAmount = [self.billAmountTextField.text floatValue];
+    
+    float tipAmount;
+    
+    if ([self.tipPercentageTextField.text floatValue]) {
+        
+        tipAmount = (billAmount * [self.tipPercentageTextField.text floatValue]);
+        
+    } else {
+        tipAmount = (billAmount * self.defaultTipPercentage);
+        }
+    
+    NSString *tip = [NSString stringWithFormat:@"Tip: $%0.2f", tipAmount];
+    //NSLog(@"Tip: $%0.2f", tipAmount);
+    
+    self.tipAmountLabel.text = tip;
+    
 }
 
 
